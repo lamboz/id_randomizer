3bed0939418fbf8b1b4c13855d4314d45cb03447
Pablo Cordero <pablocordero@gmail.com>
2019-02-12 02:27:36 -0600

    wip

--- lib/id_randomizer.rb
+++ lib/id_randomizer.rb
@@ -1,4 +1,4 @@
 require 'id_randomizer/generator'
-require 'id_randomizer/acts_as_random_id'
+require 'id_randomizer/acts_as_id_randomizer'
 
 ActiveRecord::Base.send(:include, IdRandomizer::ActsAsIdRandomizer)
